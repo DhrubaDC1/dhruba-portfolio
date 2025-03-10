4bbd0e7a57f9399e7e574d6345bd366d6350bd97
Dhruba Chakraborty <dhruba.chakraborty.2000@gmail.com>
2025-03-10 21:08:12 +0600

    Enhance Avatar component with animation and blinking effects for improved 3D model interaction

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,16 +1,94 @@
 import React from "react";
-import { Canvas } from "@react-three/fiber";
+import { Canvas, useFrame } from "@react-three/fiber";
 import { OrbitControls, useGLTF } from "@react-three/drei";
-
+import { useEffect, useRef } from "react";
+import * as THREE from "three";
 // Avatar Component to Load the 3D Model
-function Avatar() {
-  const gltf = useGLTF("dhruba_model_hqh.glb");
-  const model = gltf.scene;
-  model.position.y = -6; // Adjust position if needed
-  // model.rotation.x = 1;
-  return <primitive object={model} scale={[5, 5, 5]} />; // Adjust scale if needed
-}
+function Avatar(props) {
+  const groupRef = useRef();
+  const mixerRef = useRef();
+  const { scene, animations } = useGLTF("/dhruba.glb");
+  scene.position.setY(-5);
+  scene.scale.set(3, 3, 3);
+  let blinkVal = 0;
+  let blinkDir = 1;
+  let stareDuration = 0;
 
+  // Setup model properties
+  useEffect(() => {
+    scene.traverse((object) => {
+      if (object.isMesh) {
+        object.castShadow = true;
+      }
+      if (object.isSkinnedMesh) {
+        object.skeleton.bones.forEach((bone) => {
+          bone.scale.set(1, 1, 1);
+        });
+      }
+    });
+    // Setup animations
+    if (animations?.length) {
+      mixerRef.current = new THREE.AnimationMixer(scene);
+      mixerRef.current.clipAction(animations[2]).play();
+    }
+
+    // Cleanup
+    return () => {
+      if (mixerRef.current) {
+        mixerRef.current.stopAllAction();
+        mixerRef.current.uncacheRoot(scene);
+      }
+    };
+  }, [scene, animations]);
+  let eyeLidLeftID =
+    scene.getObjectByName("Wolf3D_Avatar").morphTargetDictionary[
+      "eyeBlinkLeft"
+    ];
+  let eyeLidRightID =
+    scene.getObjectByName("Wolf3D_Avatar").morphTargetDictionary[
+      "eyeBlinkRight"
+    ];
+  function blink(delta) {
+    if (blinkVal >= 1) {
+      blinkDir = -1;
+    }
+    if (blinkVal == 0 && blinkDir == -1) {
+      stareDuration = 0;
+      blinkDir = 1;
+    }
+    if (blinkVal >= 0 && blinkVal <= 1) {
+      blinkVal += delta * blinkDir * 7;
+      if (blinkVal < 0) blinkVal = 0;
+      if (blinkVal > 1) blinkVal = 1;
+      scene.getObjectByName("Wolf3D_Avatar").morphTargetInfluences[
+        eyeLidLeftID
+      ] = blinkVal;
+      scene.getObjectByName("Wolf3D_Avatar").morphTargetInfluences[
+        eyeLidRightID
+      ] = blinkVal;
+    }
+  }
+
+  // Animation loop
+  useFrame((state, delta) => {
+    // Blink
+    if (stareDuration <= 3) {
+      stareDuration += delta;
+    } else {
+      blink(delta);
+    }
+
+    if (mixerRef.current) {
+      mixerRef.current.update(delta);
+    }
+  });
+
+  return (
+    <group ref={groupRef} {...props}>
+      <primitive object={scene} position={[0, -6, 0]} scale={[5, 5, 5]} />
+    </group>
+  );
+}
 function About() {
   return (
     <section
@@ -22,13 +100,13 @@ function About() {
         <div className="w-1/2 h-full flex items-center justify-center">
           <div className="w-full h-full">
             <Canvas camera={{ position: [0, 0, 5], fov: 75 }}>
-              <ambientLight intensity={0.6} />
-              <pointLight position={[5, 5, 5]} intensity={1} />
-              <pointLight
-                position={[-5, 5, -5]}
-                intensity={0.5}
+              <ambientLight intensity={1} />
+              <pointLight position={[5, 5, 2]} intensity={30} color="red" />
+              {/* <pointLight
+                position={[-10, 5, 2]}
+                intensity={1}
                 color="#00ffcc"
-              />
+              /> */}
               <Avatar />
               <OrbitControls enableZoom={false} />
             </Canvas>
